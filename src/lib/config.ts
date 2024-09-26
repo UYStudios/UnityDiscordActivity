8ef2daa41d1c2096f4ae4ae6699bf43c10e8b9b5
uyadav10 <uyadav10@gmail.com>
2024-09-26 11:49:21 +0530

    basic

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    CLIENT_ID: "1287641266284269602", // Discord Application ID
+    COMPANY_NAME: "Hopscotch", // Company or Developer Name
+    PRODUCT_NAME: "TestGame", // Game Name
+    PRODUCT_VERSION: "0.1.0", // Game Version
+};
